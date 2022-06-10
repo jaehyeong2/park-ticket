3239a138a621e17953bbf4c67d8184585ca1fa0e
jae <wogud1514@naver.com>
2022-06-11 02:00:34 +0900

    feat: SwaggerConfig 추가

---
+++ src/main/java/jjfactory/parking/global/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package jjfactory.parking.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
